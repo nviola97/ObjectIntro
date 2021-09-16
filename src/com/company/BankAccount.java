056fa8e21bbc48cc8caa4575ab38b2b31f2eaded
N1VIOLA <n1viola@student.bridgew.edu>
2021-09-16 11:32:29 -0400

    Initial commit

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class BankAccount {
+    private double balance;
+    private float interestRate;
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+    public boolean withdraw(double amount){
+        if(amount > balance){
+            return false;
+        }
+        balance = balance - amount;
+        return true;
+    }
+    public double checkBalance(){
+        return balance;
+    }
+    public void addInterest(){
+        balance = (interestRate * balance) + balance;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var myAccount = new BankAccount();
+        myAccount.deposit(1000);
+        var newBalance = myAccount.addInterest();
+
+        var succeeded = myAccount.withdraw(2000);
+        if (succeeded)
+            System.out.println("you managed to withdraw successfully");
+        else
+            System.out.println(" You don't have that much money. Balance is: "+myAccount.checkBalance());
+    }
+}
