ca173c3a713d5e55aa7ea327cbdc42dce8231419
N1VIOLA <n1viola@student.bridgew.edu>
2021-09-23 11:17:14 -0400

    Select customer begin

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,4 +1,52 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Bank {
+    private ArrayList<BankAccount> allAccounts;
+    private ArrayList<Customer> allCustomers;
+    public Bank(){
+        allAccounts = new ArrayList<BankAccount>();
+        allCustomers = new ArrayList<Customer>();
+    }
+    public void doBanking(){
+        var menuReader = new Scanner(System.in);
+        while(true){
+            printMenu();
+            var userChoice = menuReader.nextInt();
+            switch (userChoice){
+                case 1:
+                    System.exit(0);
+                case 2:
+                    addCustomer(menuReader);
+                    break;
+                case 3:
+                    Customer current = selectCustomer(menuReader);
+                    break;
+                default:
+                    System.out.println("Please choose one of the menu options");
+            }
+        }
+    }
+
+    private Customer selectCustomer(Scanner reader) {
+    }
+
+    private void printMenu() {
+        System.out.println("what would you like to do next?");
+        System.out.println("    [1] Exit Program");
+        System.out.println("    [2] Add Customer");
+        System.out.println("    [3] Select customer by ID");
+        System.out.print(" Enter Choice:");
+    }
+    private void addCustomer(Scanner inputReader){
+        System.out.println("What is the new customer's name:");
+        inputReader.nextLine(); // eat the orphan newline from previous nextInt call
+        var customerName = inputReader.nextLine();
+        System.out.println("What is the new Customer's Tax ID:");
+        var taxId = inputReader.nextInt();
+        var newCustomer = new Customer(customerName, taxId);
+        allCustomers.add(newCustomer);
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,5 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        var myAccount = new BankAccount(2000);
-        var yourAccount = new BankAccount(2000, 0.05f);
-        myAccount.deposit(1000);
-        var newBalance = myAccount.addInterest();
-        var yourBalance = yourAccount.addInterest();
-        System.out.println("Your account with account ID: "+yourAccount.getAccountID()+ " has " + yourBalance + " after adding interest.");
-        var succeeded = myAccount.withdraw(2000);
-        if (succeeded)
-            System.out.println("you managed to withdraw successfully");
-        else
-            System.out.println(" You don't have that much money. Balance is: "+myAccount.checkBalance());
-    }
+}
 }
